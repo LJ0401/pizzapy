1363afaceae23999824fbc48330252601b647397
LJ0401 <1351507858@qq.com>
2017-03-20 14:16:36 +0800

    change and add

--- word_problem.py
+++ word_problem.py
@@ -1,18 +1,3 @@
-#Silly Strings
-#演示字符串的连接和重复
-print("You can concatenate two "+"strings with the '+' operator.")
-print("\nThis string "+"may not "+"seem terr"+"ibly impressive."\
-      +"But What" + "you don't know" + "is than\n"+ "it's one real"\
-      + "l" + "y" +"long string, created from the concatenation" \
-      + "of" +"twenty-two\n" + "different strings, broken across" \
-      + "six line." + "Now are you" + "impressed?" + "Okay,\n" \
-      + "this" + "one" +"long" + "string is now over!")
-print("\nIf you really like a string, you can repat it. For example,")
-print("who doesn't like pie? That's right, nobody. But if you really")
-print("like it, you should say it like you mean it:")
-print("Pie" * 10)
-input("\n\nPress the enter key to exit.")
-
 #Word Problems
 #演示数字和数学运算
 print("If a 2000 pound pregnant hippo gives birth to a 100 pound calf,")
