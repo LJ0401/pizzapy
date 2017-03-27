f46f5e231d2614c696b53187d61aa99eb17bb546
LJ0401 <1351507858@qq.com>
2017-03-27 22:39:26 +0800

    still

---
+++ handle_it.py
@@ -0,0 +1,23 @@
+#Handle It
+#演示异常处理
+
+#try/except
+try:
+    num = float(input("Enter a number:"))
+except:
+    print("Smoething went wrong)
+
+#指定异常类型
+try:
+    num = float(input("\nEnter a number:"))
+except ValueError:
+    print("That was not a number!")
+
+#处理多种异常类型
+print()
+for value in (None, "Hi!"):
+    try:
+        print("Attempting to convert", value, "-->", end="")
+        print(float(value))
+    except (TypeError, ValueError):
+        print("Something went wrong!")
